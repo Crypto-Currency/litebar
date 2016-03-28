b57fa101c63509e7a09802f2dd440bc0d8d45a1c
Spider <spider@DesktopWalletMaker.(none)>
2016-03-28 13:38:18 -0700

    updated version number

--- src/version.h
+++ src/version.h
@@ -11,12 +11,6 @@
 // client versioning
 //
 
-// These need to be macro's, as version.cpp's voodoo requires it
-#define CLIENT_VERSION_MAJOR       0
-#define CLIENT_VERSION_MINOR       6
-#define CLIENT_VERSION_REVISION    3
-#define CLIENT_VERSION_BUILD       0
-
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   10000 * CLIENT_VERSION_MINOR 
@@ -31,7 +25,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60001;
+static const int PROTOCOL_VERSION = 60002;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
@@ -46,5 +40,13 @@ static const int NOBLKS_VERSION_END = 32400;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
+
+// "mempool" command, enhanced "getdata" behavior starts with this version:
+static const int MEMPOOL_GD_VERSION = 60002;
+
+#define DISPLAY_VERSION_MAJOR       0
+#define DISPLAY_VERSION_MINOR       4
+#define DISPLAY_VERSION_REVISION    3
+#define DISPLAY_VERSION_BUILD       001
 
 #endif
